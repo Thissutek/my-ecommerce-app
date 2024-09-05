eed9eb52a84f22135e61076b5503cc9dcd8ad21d
unknown <jonathan_yau@outlook.com>
2024-09-04 23:27:26 -0400

    added boilerplate code for landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import {Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import Landing from "./pages/landing-page/views/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/landing" element={<Landing />} />
+      <Route path="*" element={<Navigate to="/landing" />} />
+    </Routes>
   );
 }
 
